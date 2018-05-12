ffda1d00a75014112c41deebfc02a4024ebcb382
Wilber Javier Ormeño Vera <wilber.ormeno@hotmail.com>
2018-05-12 10:21:44 -0500

    change colors

--- sidebar_deleteStudent.php
+++ sidebar_deleteStudent.php
@@ -5,9 +5,9 @@
                 <li><a href="dashboard.php"><i class="icon-chevron-right icon-large"></i><i class="icon-home icon-large"></i> Inicio</a></li>
                 <li><a href="students.php"><i class="icon-chevron-right icon-large"></i><i class="icon-group icon-large"></i> Búsqueda de alumnos</a></li>
                 <li><a href="add_student.php"><i class="icon-chevron-right icon-large"></i><i class="icon-th-list icon-large"></i> Registrar alumno</a></li>
-                <li><a href="edit_student.php"><i class="icon-chevron-right icon-large"></i><i class="icon-money icon-large"></i> Editar alumno</a></li>
-                <li><a href="printEnrollment.php"><i class="icon-chevron-right icon-large"></i><i class="icon-money icon-large"></i> Imprimir matrícula</a></li>
-                <li class="active"><a href="deleteStudent.php"><i class="icon-chevron-right icon-large"></i><i class="icon-table icon-large"></i> Borrar </a></li
+                <li><a href="edit_student.php"><i class="icon-chevron-right icon-large"></i><i class="icon-edit icon-large"></i> Editar alumno</a></li>
+                <li><a href="printEnrollment.php"><i class="icon-chevron-right icon-large"></i><i class="icon-print icon-large"></i> Imprimir matrícula</a></li>
+                <li class="active"><a href="deleteStudent.php"><i class="icon-chevron-right icon-large"></i><i class="icon-trash icon-large"></i> Borrar </a></li
             </ul>
     </center>
 </div>
