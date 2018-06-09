339b3ef4eeb0b1c7756a84321688fd0816e8e6bb
Wilber Javier Ormeño Vera <wilber.ormeno@hotmail.com>
2018-06-09 11:06:49 -0500

    cambios menu

--- sidebar_editStudent.php
+++ sidebar_editStudent.php
@@ -1,15 +1,23 @@
 <div class="span3" id="sidebar">
+
+    <!--<img src="images/.png">-->
     <center>
         <img src="images/cetapsi-sinfondo.png" id="logo" style="height: 200px">
-            <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
-                <li><a href="dashboard.php"><i class="icon-chevron-right icon-large"></i><i class="icon-home icon-large"></i> Inicio</a></li>
-                <li><a href="students.php"><i class="icon-chevron-right icon-large"></i><i class="icon-group icon-large"></i> Búsqueda de alumnos</a></li>
+        <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
+            <li><a href="dashboard.php"><i class="icon-chevron-right icon-large"></i><i class="icon-home icon-large"></i> Inicio</a></li>
+            <li><a href="secciones.php"><i class="icon-chevron-right icon-large"></i><i class="icon-th-list icon-large"></i> Secciones</a></li>
+            <li><a href="grades.php"><i class="icon-chevron-right icon-large"></i><i class="icon-th-list icon-large"></i> Grado</a></li>
+            <li><a href="elegirPeriodo.php"><i class="icon-chevron-right icon-large"></i><i class="icon-money icon-large"></i> Periodo</a></li>
+            <li class="active"><a href="#"><i class="icon-chevron-right icon-large"></i><i class="icon-group icon-large"></i> Alumnos</a></li>
+            <ul2 class="nav nav-list bs-docs-sidenav nav-collapse collapse" id="segundo" style="display: block; width: 190px">
+                <li ><a href="students.php"><i class="icon-chevron-right icon-large"></i><i class="icon-group icon-large"></i> Búsqueda de alumnos</a></li>
                 <li><a href="apoderados.php"><i class="icon-chevron-right icon-large"></i><i class="icon-group icon-large"></i> Apoderados</a></li>
-                <li ><a href="documentacion.php"><i class="icon-chevron-right icon-large"></i><i class="icon-th-list icon-large"></i> Documentación</a></li>
                 <li><a href="add_student.php"><i class="icon-chevron-right icon-large"></i><i class="icon-th-list icon-large"></i> Registrar alumno</a></li>
-                <li  class="active" ><a href="edit_student.php"><i class="icon-chevron-right icon-large"></i><i class="icon-edit icon-large"></i> Editar alumno</a></li>
-                <li><a href="printEnrollment.php"><i class="icon-chevron-right icon-large"></i><i class="icon-print icon-large"></i> Imprimir matrícula</a></li>
+                <li ><a href="documentacion.php"><i class="icon-chevron-right icon-large"></i><i class="icon-th-list icon-large"></i> Documentación</a></li>
+                <li class="active"><a href="edit_student.php"><i class="icon-chevron-right icon-large"></i><i class="icon-edit icon-large"></i> Editar alumno</a></li>
+                <li ><a href="printEnrollment.php"><i class="icon-chevron-right icon-large"></i><i class="icon-print icon-large"></i> Imprimir matrícula</a></li>
                 <li><a href="deleteStudent.php"><i class="icon-chevron-right icon-large"></i><i class="icon-trash icon-large"></i> Borrar </a></li
-            </ul>
+            </ul2>
+        </ul>
     </center>
 </div>
--- sidebar_secciones.php
+++ sidebar_secciones.php
@@ -5,10 +5,11 @@
 		<img src="images/cetapsi-sinfondo.png" id="logo" style="height: 200px">
         <ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
             <li ><a href="dashboard.php"><i class="icon-chevron-right icon-large"></i><i class="icon-home icon-large"></i> Inicio</a></li>
-            <li ><a href="students.php"><i class="icon-chevron-right icon-large"></i><i class="icon-group icon-large"></i> Alumnos</a></li>
             <li  class="active"><a href="secciones.php"><i class="icon-chevron-right icon-large"></i><i class="icon-th-list icon-large"></i> Secciones</a></li>
             <li><a href="grades.php"><i class="icon-chevron-right icon-large"></i><i class="icon-money icon-large"></i> Grados</a></li>
             <li><a href="elegirPeriodo.php"><i class="icon-chevron-right icon-large"></i><i class="icon-money icon-large"></i> Periodo</a></li>
+
+            <li ><a href="students.php"><i class="icon-chevron-right icon-large"></i><i class="icon-group icon-large"></i> Alumnos</a></li>
         </ul>
 	</center>
 </div>
